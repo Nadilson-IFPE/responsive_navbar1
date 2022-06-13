05e98762519a1dd7cdaaa32817dc258701c87754
Nadilson <nadilson@protonmail.com>
2022-06-13 00:40:50 -0300

    Navigation bar concluído

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Head } from "next/head";
+import NavBar from "../components/navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
